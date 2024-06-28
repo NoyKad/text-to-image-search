4a40a56e30deb577af6c9abef821f5ddf959aa06
Noy Kadosh <noykadosh123@gmail.com>
2024-06-28 20:35:09 +0300

    working sql to pandas

---
+++ import_sql_to_pandas.py
@@ -0,0 +1,20 @@
+import mysql.connector as connection
+import pandas as pd
+try:
+    mydb = connection.connect(
+            host="127.0.0.1", 
+            user="root",
+            password="5019",
+            database='media_tagging',
+            use_pure=True
+            )
+    
+    query = "Select * from media;"
+    result_dataFrame = pd.read_sql(query, mydb)
+
+    print(result_dataFrame.head())
+
+    mydb.close() #close the connection
+except Exception as e:
+    mydb.close()
+    print(str(e))
