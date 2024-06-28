9acea1eb404554ce10f3493fb181326d0e65d456
Noy Kadosh <noykadosh123@gmail.com>
2024-06-28 20:48:10 +0300

    data to csv

--- import_sql_to_pandas.py
+++ import_sql_to_pandas.py
@@ -12,7 +12,7 @@ try:
     query = "Select * from media;"
     result_dataFrame = pd.read_sql(query, mydb)
 
-    print(result_dataFrame.head())
+    result_dataFrame.to_csv('./data/media.csv')
 
     mydb.close() #close the connection
 except Exception as e:
